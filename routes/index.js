91235eee539a2aaa3ded00c77858312e674130b4
Gu_Siyi <stiles1024@qq.com>
2021-08-23 17:31:15 +0800

    v1.3.0 模拟拨打，自动添加黑名单，黑名单自动屏蔽

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+//let server_host = '10.70.156.105:8080'
+let server_host = 'localhost:8080'
+
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { server: 'Express' });
   });
   app.get('/users', function (req, res) {
     res.render('error', { message:'hello', error:{status: '1', stack: '2'} });
   });
   app.get('/login', function (req, res) {
-    res.render('adminlogin');
+    res.render('adminlogin', { server_host: server_host });
   });
   app.get('/success', function (req, res) {
-    res.render('success');
+    res.render('success', { server: server_host});
   });
-  app.get('/config' ,function(req, res) {
-    res.render('modelConfig')
+  app.get('/base_config' ,function(req, res) {
+    res.render('baseModelConfig', { server_host: server_host })
   })
+
 };
