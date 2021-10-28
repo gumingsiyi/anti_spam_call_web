c9f792c1cbd32025bf14f1fd65736297ec79c36c
Gu_Siyi <stiles1024@qq.com>
2021-10-28 15:15:10 +0800

    v1.4.3 添加注释

--- routes/admin.js
+++ routes/admin.js
@@ -4,21 +4,22 @@ var router = express.Router();
 //let server_host = '10.70.156.105:8080';
 let server_host = 'localhost:8080';
 
-router.get('/', function (req, res) {
-  res.render('index', { server: 'Express' });
-});
-router.get('/users', function (req, res) {
-  res.render('error', { message:'hello', error:{status: '1', stack: '2'} });
-});
+//登录页面
 router.get('/login', function (req, res) {
   res.render('admin/login', { server_host: server_host });
 });
+
+//后台控制模拟拨打等操作页面
 router.get('/success', function (req, res) {
   res.render('admin/success', { server_host: server_host});
 });
+
+//基础模型设置页面
 router.get('/base_config' ,function(req, res) {
   res.render('admin/baseModelConfig', { server_host: server_host })
 })
+
+//详细信息查看页面
 router.get('/details', function(req, res) {
   res.render('admin/details', { server_host: server_host })
 })
